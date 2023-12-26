eca9fdc6a6812f9fb5a418dd141f31b53fad8c05
BigggZ123 <2547462414@qq.com>
2023-12-27 00:57:35 +0800

    log for NodeIter's traverse like stl

--- main.cpp
+++ main.cpp
@@ -16,16 +16,16 @@ auto visit (NodeIter<T>* node) -> bool{
 };
 
 int main() {
-//    vector<int> v ;
-//    for (int i = 0 ; i < 7 ;  i ++){
-//        v.push_back(i + 1);
-//    }
-//    CompleteTree<int> ct ;
-//    for (int i =  0 ; i < 100 ; i ++){
-//        ct.insert(i + 1) ;
-//    }
-//    auto it = ct.root() ;
-////    cout << ct.root()->LChild()->LChild()->parent()->parent()->data() << endl ;
-//    ct.preOrder(visit);
-    BinarySearchTree<int> bst ;
+    vector<int> v ;
+    for (int i = 0 ; i < 7 ;  i ++){
+        v.push_back(i + 1);
+    }
+    CompleteTree<int> ct ;
+    for (int i =  0 ; i < 100 ; i ++){
+        ct.insert(i + 1) ;
+    }
+    auto it = ct.root() ;
+//    cout << ct.root()->LChild()->LChild()->parent()->parent()->data() << endl ;
+    ct.preOrder(visit);
+//    BinarySearchTree<int> bst ;
 }

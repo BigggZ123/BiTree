a9b051d1394b939dd8bd6c3b35b8f475dfcf0c7b
BigggZ123 <2547462414@qq.com>
2023-12-27 00:53:23 +0800

    专门为iter添加了相关的方法

--- NodeIter.h
+++ NodeIter.h
@@ -4,6 +4,7 @@
 
 #ifndef BITREE_NODEITER_H
 #define BITREE_NODEITER_H
+#include <queue>
 
 template <typename T>
 class NodeIter{
@@ -19,5 +20,106 @@ public:
     virtual bool        setLChild   (T data)            {return false ;}
 };
 
+
+template <typename T>
+bool PreOrder(NodeIter<T>* node , bool(*visit)(NodeIter<T>*)){
+    //对空也应当执行访问操作
+    if (not node){
+        if (not visit(node))
+            return false ;
+        return true ;
+    }
+
+    if (not visit(node))
+        return false ;
+    if (not PreOrder(node->LChild() , visit))
+        return false ;
+    if (not PreOrder(node->RChild() , visit))
+        return false ;
+    return true ;
+}
+
+template <typename T>
+bool InOrder(NodeIter<T>* node , bool(*visit)(NodeIter<T>*)){
+    if (not node){
+        if (not visit(node))
+            return false ;
+        return true ;
+    }
+
+    if (not InOrder(node->LChild() , visit))
+        return false ;
+    if (not visit(node))
+        return false ;
+    if (not InOrder(node->RChild() , visit))
+        return false ;
+    return true ;
+}
+
+template <typename T>
+bool PostOrder(NodeIter<T>* node , bool(*visit)(NodeIter<T>*)) {
+    if (not node) {
+        if (not visit(node))
+            return false;
+        return true;
+    }
+
+    if (not PostOrder(node->LChild(), visit))
+        return false;
+    if (not PostOrder(node->RChild(), visit))
+        return false;
+    if (not visit(node))
+        return false;
+    return true;
+}
+
+template <typename T>
+bool LevelOrder(NodeIter<T>* node , bool (*visit)(NodeIter<T>*)){
+    if (not node)
+        return true ;
+
+    std::queue<NodeIter<T>*> nodes ;
+    nodes.push(node);
+    while (not nodes.empty()){
+        NodeIter<T>* cur = nodes.front();
+        nodes.pop();
+
+        if (not visit(cur))
+            return false ;
+        //因为有时会对NULL也执行访问操作
+        //故nullptr有必要入队列
+        if (cur != nullptr){
+            nodes.push(cur->LChild());
+            nodes.push(cur->RChild());
+        }
+
+    }
+}
+
+template <typename T>
+int Height(NodeIter<T>* root){
+    if (not root) return -1 ;
+    if (not root->LChild() and not root->RChild()) return 0 ;
+
+    return std::max(
+            Height(root->LChild()),
+            Height(root->RChild())
+            )
+            + 1 ;
+}
+
+template <typename T>
+int Depth(NodeIter<T>* root , NodeIter<T>* node){
+    if (root == nullptr or node == nullptr) return -1 ;
+    if (root == node) return 0 ;
+
+    int leftDepth = Depth(root->LChild() , node);
+    if (leftDepth != -1) return leftDepth + 1 ;
+
+    int rightDepth = Depth(root->RChild() , node) ;
+    if (rightDepth != -1) return rightDepth + 1 ;
+
+    return -1 ;
+}
 
 #endif //BITREE_NODEITER_H
--- main.cpp
+++ main.cpp
@@ -7,25 +7,26 @@ using namespace std ;
 
 
 int main() {
-//    vector<BiTreeNode<int>*> nodes ;
-//    for (int i = 0 ; i < 10 ; i ++){
-//        nodes.push_back(new BiTreeNode<int>(i));
-//    }
-//    BiTree<int> bt(nodes[0]);
-//    bt.root()->setLChild(10);
-//    bt.root()->setRChild(20);
-//    bt.root()->LChild()->setLChild(30);
-//    bt.root()->LChild()->LChild()->setLChild(40);
-////    auto it = (BiTree<int>::iterator*)bt.root() ;
-////    cout << it->base()->LChild->LChild->data << endl ;
-////    NodeIter<int>* it = bt.root()->LChild()->LChild() ;
-////    cout << it->data() << endl ;
-//    bt.preOrder([](NodeIter<int> * iter)->bool {
-//        if (not iter)
-//            return true ;
-//        else
-//            cout <<  iter->data() << ":\t" <<iter->height() << endl ;
-//        return true ;
-//    });
+
+    vector<BiTreeNode<int>*> nodes ;
+    for (int i = 0 ; i < 10 ; i ++){
+        nodes.push_back(new BiTreeNode<int>(i));
+    }
+    BiTree<int> bt(nodes[0]);
+    bt.root()->setLChild(10);
+    bt.root()->setRChild(20);
+    bt.root()->LChild()->setLChild(30);
+    bt.root()->LChild()->LChild()->setLChild(40);
+//    auto it = (BiTree<int>::iterator*)bt.root() ;
+//    cout << it->base()->LChild->LChild->data << endl ;
+//    NodeIter<int>* it = bt.root()->LChild()->LChild() ;
+//    cout << it->data() << endl ;
+    bt.preOrder([](NodeIter<int> * iter)->bool {
+        if (not iter)
+            return true ;
+        else
+            cout <<  iter->data() << ":\t" <<iter->height() << endl ;
+        return true ;
+    });
 
 }
